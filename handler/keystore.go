99e50e516c97d898fbf662c195df441ee8b74a52
michaelyang <albertyang123456@gmail.com>
2022-11-08 16:11:21 +0800

    AddressVerify

--- handler/keystore.go
+++ handler/keystore.go
@@ -1,1 +1,37 @@
 package handler
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func CreateKeyStore(password string, dir string) {
+	ks := keystore.NewKeyStore(dir, keystore.StandardScryptN, keystore.StandardScryptP)
+	account, err := ks.NewAccount(password)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(account.Address.Hex())
+}
+
+func ImportKs(fileDir string, password string) {
+	ks := keystore.NewKeyStore("./tmp", keystore.StandardScryptN, keystore.StandardScryptP)
+	jsonBytes, err := ioutil.ReadFile(fileDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	account, err := ks.Import(jsonBytes, password, password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(account.Address.Hex())
+
+	if err = os.Remove(fileDir); err != nil {
+		log.Fatal(err)
+	}
+}
