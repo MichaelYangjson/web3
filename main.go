6d7a1caa510ce6f8ca9d8b916b87860c04f3a5c9
michaelyang <albertyang123456@gmail.com>
2022-11-08 14:54:00 +0800

    GetWallet

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"context"
+	"eth_test/handler"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 )
 
-func main ()  {
+func main() {
 	client, err := ethclient.Dial("https://cloudflare-eth.com")
 	if err != nil {
 		log.Fatal(err)
@@ -16,4 +17,8 @@ func main ()  {
 	account := common.HexToAddress("0x47e31E940f2d1F8FeC009d07e266F893db154A53")
 	balance, err := client.BalanceAt(context.Background(), account, nil)
 	fmt.Println(balance)
+
+	var wallet handler.Wallet
+	prikey, publikey, address := wallet.GetWallet()
+	fmt.Println("prikey:" + prikey + " publickey:" + publikey + " address:" + address)
 }
